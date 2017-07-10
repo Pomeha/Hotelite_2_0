7f1f84fa4fef26d497fc027c0f7275430afe9257
Vladislav <mrjordanvl@gmail.com>
2017-07-10 17:02:12 +0300

    Remove API for better times

--- app/controllers/api/v1/hotels_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::HotelsController < ApplicationController
-  respond_to :json
-  serialization_scope :view_context
-
-  def index
-    hotels = Hotel.all
-    render json: hotels, status: 200, each_serializer: HotelSerializer
-  end
-end
--- app/serializers/hotel_serializer.rb
+++
@@ -1,3 +0,0 @@
-class HotelSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :star, :room, :created_at
-end
