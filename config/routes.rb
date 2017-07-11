ee4b8d458ee54026a65d0b33e47567fc96be4c39
Vladislav <mrjordanvl@gmail.com>
2017-07-11 03:54:56 +0300

    [Add] Admin, without role

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :hotels
+    resources :users
   end
+
   devise_for :users
+  scope "/admin" do
+    resources :users
+  end
+  #devise_for :users
   resources :hotels do
     resources :reviews
     collection do
